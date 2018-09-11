8087ddbd97ab1308db620015dbca0987daa500a8
Alonso Godinez <alonxogs@gmail.com>
2018-09-11 11:07:29 -0500

    asdfsa

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const { IncomingWebhook, WebClient } = require('@slack/client');
 const url = "https://hooks.slack.com/services/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";
 const webhook = new IncomingWebhook(url);
 
-const token = "xoxp-429168385154-429046675235-433154998660-8cedb0409c4e15c5276f14a267407619";
+const token = "xoxp-429168385154-429046675235-434714082822-05458fe076231fc6af0625ea895325fa";
 const channelID = "CCMEC99S5";
 
 const web = new WebClient(token);
@@ -50,7 +50,7 @@ function WHTest (req, res, next) {
 
 
   web.chat.postMessage({
-    token,
+    token: "Ish4vtquPrFbPXU05VcVW3Nj",
     channel: channelID,
     text: "Probando",
     "attachments": [
